c6503ab30d011161b6b4cdb8763893418fd8e02c
luciaguti8 <luciaguti4design@gmail.com>
2024-11-10 19:41:19 +0100

    loop_carruselAnonimos

--- js/script.js
+++ js/script.js
@@ -247,6 +247,8 @@ document.addEventListener('DOMContentLoaded', function() {
         slidesPerView: 2,
         spaceBetween: 30,
         slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: true,
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
